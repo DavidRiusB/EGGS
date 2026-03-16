b5f23412a498d6cec0d4d621b71f236f6f69c3e5
David rius <davidriusbazurdo@gmail.com>
2026-03-16 13:37:51 -0500

    feat: setup environment config, database connection and base modules
    - configured environment variables with .env and ConfigModule
    - connected PostgreSQL database using TypeORM
    - installed and configured TypeORM
    - created base module scaffolding for users, auth, and repairs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { RepairsModule } from './modules/repairs/repairs.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+TypeOrmModule.forRootAsync({
+inject:[ConfigService],
+useFactory: (config: ConfigService)=> ({
+      type: 'postgres',
+      host: config.get('DB_HOST'),
+      port: config.get<number>('DB_PORT'),
+      username: config.get('DB_USERNAME'),
+      password: config.get('DB_PASSWORD'),
+      database: config.get('DB_DATABASE'),
+      autoLoadEntities: true,
+      synchronize: true,}),
+}),
+UserModule,
+AuthModule,
+RepairsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService)
+  console.log("ENV DB_HOST:", configService.get("DB_HOST"));
+  console.log("ENV PORT:", configService.get("PORT"));
+  await app.listen(process.env.PORT ?? 3000);  
+
 }
 bootstrap();
